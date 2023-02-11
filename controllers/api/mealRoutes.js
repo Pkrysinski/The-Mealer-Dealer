93587d3aee0a1e862d93d6ee348a8edab740934e
Joseph Hayes <joey.hayes.1996@gmail.com>
2023-02-11 16:20:00 -0700

    Got basic meal_results query working. Needs the meal_results handlebars page to be complete in order to further test.

--- controllers/api/mealRoutes.js
+++ controllers/api/mealRoutes.js
@@ -5,7 +5,7 @@ const sequelize = require('sequelize');
 //Build meal routes
 
 //Get meal results
-router.get('/meal_results', async (req, res) => {
+router.post('/meal_results', async (req, res) => {
     try {
         if (req.body.mainProtein === "Any") req.body.mainProtein = "*";
         const mealResults = await Recipe.findAll({
@@ -16,7 +16,7 @@ router.get('/meal_results', async (req, res) => {
                     //     where: {sequelize.col('prepTime'), '+', sequelize.col('cookTime') },
                     //     'totalTime')
                     // ]
-                    [sequelize.literal('(prepTime + cookTime)'),
+                    [sequelize.literal('(prep_time + cook_time)'),
                         'totalTime'
                     ]
                 ]
@@ -24,18 +24,19 @@ router.get('/meal_results', async (req, res) => {
             //filters out the meals by main protein, then by getting a less-than or equal-to comparison to the above summed total meal cook time.
             where: {
                 mainProtein: req.body.mainProtein,
-                totalTime: {
-                    [sequelize.lte]: req.body.totalTime,
-                }
+                // totalTime: {
+                //     [sequelize.lte]: req.body.totalTime,
+                // }
             }
         });
 
         const meals = mealResults.map((meal) =>
             meal.get({ plain: true }));
 
-        res.render('meal_results', {
-            meals,
-        });
+            res.json(meals);
+        // res.render('meal_results', {
+        //     meals,
+        // });
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
