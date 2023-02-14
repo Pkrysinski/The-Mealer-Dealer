eadc7a6917ae810ea4426bb14fd19281b8309128
Peter Krysinski <pakrysin@gmail.com>
2023-02-13 20:17:36 -0700

    Changing column names from camelCase to underscored, updating mealRoutes.js logic so include the withAuth on the POST for /meal_results, and adding a little more logic to the buildmeal.js for the button handler to actually grab the values entered by the user

--- public/js/buildmeal.js
+++ public/js/buildmeal.js
@@ -1,8 +1,25 @@
 const submitMealButtonHandler = async (event) => {
 
-    event.preventDefault();
+    // event.preventDefault();
+    // document.location.replace('/api/meals/meal_results');
 
-    document.location.replace('/api/meals/meal_results');
+    const quantityOfMeals = document.querySelector('#quantityOfMeals').value.trim();
+    const quantityOfPeople = document.querySelector('#quantityOfPeople').value.trim();
+    const main_protein = document.querySelector('#main_protein').value.trim();
+  
+    if (quantityOfMeals && quantityOfPeople && main_protein) {
+      const response = await fetch('/api/meals/meal_results', {
+        method: 'POST',
+        body: JSON.stringify({  quantityOfMeals, quantityOfPeople, main_protein }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/api/meals/meal_results');
+      } else {
+        alert(response.statusText);
+      }
+    }    
 
 };
 
