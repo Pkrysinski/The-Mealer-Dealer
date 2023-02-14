fcfb1beef0cf79307d1ec7137aad8967aa36cbcf
Joseph Hayes <joey.hayes.1996@gmail.com>
2023-02-13 21:41:35 -0700

    Adjusted meal_results endpoint to use params instead of body

--- controllers/api/mealRoutes.js
+++ controllers/api/mealRoutes.js
@@ -24,36 +24,38 @@ router.get('/build_your_meal', withAuth, async (req, res) => {
 });
 
 //Get meal results
-router.post('/meal_results', withAuth, async (req, res) => {
+router.get('/meal_results/:total_time&:main_protein', withAuth, async (req, res) => {
     try {
 
       // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Recipe }],
-      });
-      const user = userData.get({ plain: true });
+      // const userData = await User.findByPk(req.session.user_id, {
+      //   attributes: { exclude: ['password'] },
+      //   include: [{ model: Recipe }],
+      // });
+      // const user = userData.get({ plain: true });
 
-        if (req.body.main_protein === "Any") req.body.main_protein = "*";
+        if (req.params.main_protein === "Any") req.params.main_protein = "*";
+        req.params.total_time = parseInt(req.params.total_time);
+        console.log(req.params);
         const mealResults = await Recipe.findAll({
             //includes a column that has the summed prep and cook time for each meal
-            attributes: {
-                include: [
-                    //     [ sequelize.fn('SUM', 
-                    //     where: {sequelize.col('prep_time'), '+', sequelize.col('cook_time') },
-                    //     'totalTime')
-                    // ]
-                    [sequelize.literal('(prep_time + cook_time)'),
-                        'totalTime'
-                    ]
-                ]
-            },
+            // attributes: {
+            //     include: [
+            //         //     [ sequelize.fn('SUM', 
+            //         //     where: {sequelize.col('prep_time'), '+', sequelize.col('cook_time') },
+            //         //     'totalTime')
+            //         // ]
+            //         [sequelize.literal('(prep_time + cook_time)'),
+            //             'totalTime'
+            //         ]
+            //     ]
+            // },
             //filters out the meals by main protein, then by getting a less-than or equal-to comparison to the above summed total meal cook time.
             where: {
-                main_protein: req.body.main_protein,
-                // totalTime: {
-                //     [sequelize.lte]: req.body.totalTime,
-                // }
+                main_protein: req.params.main_protein,
+                total_time: {
+                    [sequelize.lte]: req.params.total_time,
+                }
             }
         });
 
@@ -61,9 +63,8 @@ router.post('/meal_results', withAuth, async (req, res) => {
             meal.get({ plain: true }));
 
         res.render('meal_results', {
-            ...user,
-            logged_in: req.session.logged_in,            
             meals,
+            logged_in: req.session.logged_in,            
         });
     } catch (err) {
         console.log(err);
