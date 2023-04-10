3c023c63c3bb8519e578a21c8f19bf0bf21d5e64
brachtco <brachtdanny@gmail.com>
2023-04-10 14:06:15 -0600

    Adding to my repos

--- seeds/seed.js
+++ seeds/seed.js
@@ -29,3 +29,54 @@ const seedDatabase = async () => {
 seedDatabase();
 
 // www.themealdb.com/api/json/v1/1/random.php for random recipes to seed. Missing servings and cook/prep time.
+
+
+[
+  {
+      "name": "University of Utah",
+      "image": "",
+      "region": "Western"
+  },
+
+  {
+      "name": "San Diego State University",
+      "image": "",
+      "region": "Western"
+  },
+
+  {
+      "name": "University of Oregon",
+      "image": "",
+      "region": "Western"
+  },
+
+  {
+      "name": "Arizona State University",
+      "image": "",
+      "region": "Western"
+  },
+
+  {
+      "name": "Boston College",
+      "image": "",
+      "region": "Eastern"
+  },
+
+  {
+      "name": "Florida State University",
+      "image": "",
+      "region": "Eastern"
+  },
+
+  {
+      "name": "Harvard",
+      "image": "",
+      "region": "Eastern"
+  },
+
+  {
+      "name": "Duke",
+      "image": "",
+      "region": "Eastern"
+  },
+]
